10e7f223b6f5e4160a47935f06bfca0c2f4a6ca5
hogang <hyogangster1@gmail.com>
2023-10-13 21:21:26 +0900

    함수 인자로 받아서 계산

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -2,34 +2,18 @@ import UIKit
 
 // 계산기 구현하기(사칙연산 간단하게)
 
-enum Operator: String {
-    case add = "+"
-    case substract = "-"
-    case multiple = "*"
-    case divide = "/"
-}
-
 // 일급객체로서의 함수로 구현
-
+// 클로저
 let add: (Int, Int) -> Int = { $0 + $1 }
 let subtract: (Int, Int) -> Int = { $0 - $1 }
 let multiple: (Int, Int) -> Int = { $0 * $1 }
 let divide: (Int, Int) -> Int = { $0 / $1 }
 
-func calculator(op: Operator) -> (Int, Int) -> Int {
-    switch op {
-    case .add:
-        return add
-    case .substract:
-        return subtract
-    case .multiple:
-        return multiple
-    case .divide:
-        return divide
-    }
-}
 
-calculator(op: .add)(5,6)
+func calculator(_ a: Int, _ b: Int, op: (Int, Int) -> Int) -> Int {
+    return op(a,b)
+}
+print(calculator(1, 2, op: add))
 
 /*
 
