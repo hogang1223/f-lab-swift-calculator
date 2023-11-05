0212ce1c96616849ea5780f909d949401923cc37
hogang <hyogangster1@gmail.com>
2023-11-05 16:01:08 +0900

    CommandPattern으로 계산기 구현

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,19 +1,56 @@
 import UIKit
+import Foundation
 
 // 계산기 구현하기(사칙연산 간단하게)
 
-// 일급객체로서의 함수로 구현
-// 클로저
-let add: (Int, Int) -> Int = { $0 + $1 }
-let subtract: (Int, Int) -> Int = { $0 - $1 }
-let multiple: (Int, Int) -> Int = { $0 * $1 }
-let divide: (Int, Int) -> Int = {
-    guard $1 != 0 else { return 0 }
-    return $0 / $1
+// Command Pattern으로 구현
+
+protocol Command {
+    func calculate(_ num1: Int, _ num2: Int)
 }
 
-
-func calculator(_ a: Int, _ b: Int, op: (Int, Int) -> Int) -> Int {
-    return op(a,b)
+class Add: Command {
+    func calculate(_ num1: Int, _ num2: Int) {
+        print("\(num1) + \(num2) = \(num1 + num2)")
+    }
 }
-print(calculator(1, 0, op: divide))
+
+class Subtract: Command {
+    func calculate(_ num1: Int, _ num2: Int) {
+        print("\(num1) - \(num2) = \(num1 - num2)")
+    }
+}
+
+class Multiply: Command {
+    func calculate(_ num1: Int, _ num2: Int) {
+        print("\(num1) X \(num2) = \(num1 * num2)")
+    }
+}
+
+class Divide: Command {
+    func calculate(_ num1: Int, _ num2: Int) {
+        guard num2 != 0 else {
+            print("\(num1) / \(num2) = 0")
+            return
+        }
+        print("\(num1) / \(num2) = \(num1 / num2)")
+    }
+}
+
+class Calculator {
+    private var command: Command
+    init(command: Command) {
+        self.command = command
+    }
+    
+    func calculating(_ num1: Int, _ num2: Int) {
+        self.command.calculate(num1, num2)
+    }
+    
+}
+
+let add = Add()
+let subtract = Subtract()
+let multiply = Multiply()
+let divide = Divide()
+Calculator(command: add).calculating(3, 5)
